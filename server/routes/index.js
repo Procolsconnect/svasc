3ac43b29a76db6aa6c8830c8adcba9461379797d
NavinArunachalam <navinarunachalam20@gmail.com>
2026-02-12 22:07:12 +0530

    feat: implement academic programs API with CRUD operations and grouped retrieval, including a Postman collection.

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,10 +6,13 @@ const activitiesRoutes = require('./activities.route');
 const blogsRoutes = require('./blogs.route');
 const eventsRoutes = require('./events.route');
 
+const programsRoutes = require('./programs.route');
+
 router.use('/alumni', alumniRoutes);
 router.use('/home', homeRoutes);
 router.use('/activities', activitiesRoutes);
 router.use('/blogs', blogsRoutes);
 router.use('/events', eventsRoutes);
+router.use('/programs', programsRoutes);
 
 module.exports = router;
